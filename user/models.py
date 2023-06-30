37cf79e17af0ebe9d3a754e3439187f52e66782c
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-06-30 14:51:34 +0200

    create user creation tests

--- user/models.py
+++ user/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class SoftdeskUser(User):
     """ Custom user, with attributes specific to the softdesk application """
     age = models.IntegerField()
-    can_be_contacted = models.BooleanField()
-    can_data_be_shared = models.BooleanField()
+    can_be_contacted = models.BooleanField(default=False)
+    can_data_be_shared = models.BooleanField(default=False)
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from user.models import SoftdeskUser
 
-# Create your tests here.
+
+class TestSoftdeskUser(APITestCase):
+
+    url = "/users/"
+
+    def setUp(self) -> None:
+        self.user = SoftdeskUser.objects.create_user(
+            username="admin",
+            password="password",
+            age=27,
+        )
+
+        self.client.force_login(self.user)
+
+    def test_create_user(self):
+
+        response = self.client.post(self.url, data={
+            "username": "new_user",
+            "password": "password",
+            "age": 15,
+        })
+
+        self.assertIn(response.status_code, [200, 201], response.json())
+        self.assertNotEqual(SoftdeskUser.objects.get(username="new_user"), None)
+
+    def test_create_user_less_than_15_years_old(self):
+
+        response = self.client.post(self.url, data={
+            "username": "new_user",
+            "password": "password",
+            "age": 12,
+        })
+
+        self.assertEqual(response.status_code, 400, response.json())
+        self.assertFalse(SoftdeskUser.objects.filter(username="new_user").exists())
