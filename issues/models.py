28197a23794f20b214ca41e020d7d11786b9e326
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2023-07-13 17:38:14 +0200

    update relation between issue and users

--- issues/models.py
+++ issues/models.py
@@ -1,19 +1,25 @@
 from django.utils.translation import gettext_lazy as _
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from projects.models import Project, Contributor
+from projects.models import Project
+from user.models import SoftdeskUser
 
 
 class Issue(models.Model):
 
     class IssueTag(models.TextChoices):
         BUG = "BUG", _("bug")
-        TODO = "TODO", _("todo")
+        TASK = "TASK", _("task")
         FEATURE = "FEATURE", _("feature")
 
     class IssueState(models.TextChoices):
+        TODO = "TODO", _("todo")
         IN_WORK = "INWORK", _("in_work")
         RELEASED = "RELEASED", _("released")
+
+    class IssuePriority(models.TextChoices):
+        LOW = "LOW", _("low")
+        MEDIUM = "MEDIUM", _("medium")
+        HIGH = "HIGH", _("high")
 
     created_time = models.DateTimeField(auto_now=True)
 
@@ -25,7 +31,7 @@ class Issue(models.Model):
     state = models.CharField(
         max_length=10,
         choices=IssueState.choices,
-        default=IssueState.IN_WORK
+        default=IssueState.TODO
     )
 
     title = models.CharField(
@@ -37,12 +43,10 @@ class Issue(models.Model):
         default=""
     )
 
-    priority = models.SmallIntegerField(
-        default=5,
-        validators=[
-            MinValueValidator(0),
-            MaxValueValidator(5)
-        ]
+    priority = models.CharField(
+        max_length=10,
+        choices=IssuePriority.choices,
+        default=IssuePriority.LOW
     )
 
     project = models.ForeignKey(
@@ -51,14 +55,35 @@ class Issue(models.Model):
     )
 
     author = models.ForeignKey(
-        to=Contributor,
+        to=SoftdeskUser,
         on_delete=models.CASCADE,
         related_name='issue_author'
     )
 
     assigned_user = models.ForeignKey(
-        to=Contributor,
+        to=SoftdeskUser,
         on_delete=models.CASCADE,
         null=True,
         related_name='issue_assigned_user'
     )
+
+
+""" class Comment(models.Model):
+
+    issue = models.ForeignKey(
+        to=Issue,
+        on_delete=models.CASCADE
+    )
+
+    author = models.ForeignKey(
+        to=Contributor,
+        on_delete=models.CASCADE
+    )
+
+    description = models.CharField(
+        max_length=2000
+    )
+
+    created_time = models.DateTimeField(
+        auto_now=True
+    ) """
